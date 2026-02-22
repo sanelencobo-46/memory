2348bf695dae68b56d76d37cfdff1fd29be2cf50
Sanele Ncobo <sncobo461@gmail.com>
2026-02-22 19:56:40 +0200

    Made changes on index.html aand script.js to improve the functionality of the game. Improved style.css to get a better style game layout.

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
+const gameBoard = document.getElementById("gameBoard");
+
+const values = [1, 1, 2, 2, 3, 3]
+
+// Shuffle the numbers
+values.sort(() => 0.5 - Math.random());
+
 let firstCard = null;
 let secondCard = null;
 let lockBoard = false;
 
-document.querySelectorAll(".card").forEach(card => {
-    card.addEventListener("click", () => {
-        // 1. When a User clicks any card
+// Create cards
+values.forEach(value => {
+   const card = document.createElement("div");
+   card.classList.add("card");
+   card.dataset.value = value;
 
-        if (lockBoard || card === firstCard) return;
+card.innerHTML = `
+<div class="card-inner">
+<div class="card-front"></div>
+<div class="cardr-back ${getColorClass(value)}">
+${value}
+</div>
+</div>
+`;
 
-        card.style.visibility = "hidden"
+gameBoard.appendChild(card);
 
-        if (!firstCard) {
-            firstCard = card;
+card.addEventListener("click", () => {
+    if (lockBoard) return;
+    if (card === firstCard) return;
+
+    card.classList.add("flip");
+
+    if (!firstCard) {
+        firstCard = card;
+    } else {
+        secondCard = card;
+        lockBoard = true;
+
+        if (firstCard.dataset.value === secondCard.dataset.value) {
+            resetTurn();
         } else {
-            secondCard = card;
-
-            if (firstCard.dataset.value === secondCard.dataset.value) {
-                firstCard = null;
-                secondCard = null;
-
-            } else {
-                lockBoard = true;
-
-                setTimeout(() => {
-                    firstCard.style.visibility = "visible";
-                    secondCard.style.visibility = "visible";
-                    firstCard = null;
-                    secondCard = null;
-                    lockBoard = false;
-                }, 800);
-            }
+            setTimeout(() => {
+                firstCard.classList.remove("flip");
+                secondCard.classList.remove("flip");
+                resetTurn();
+            }, 1000);
         }
-     });
-});
+      }
+    });
+ });
+
+ function resetTurn() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+ }
+
+ function getColorClass(value) {
+    if (value == 1) return "green";
+    if (value == 2) return "orange";
+    if (value == 3) return "blue";
+ }
